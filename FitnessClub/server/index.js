8d8488c82bfabee04d97179300a2b81702f28ed0
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-11-28 12:16:53 +0300

    Добавил подключение к бд

--- FitnessClub/server/index.js
+++ FitnessClub/server/index.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import { UserController, GymController, MembershipController, NewsController } from './controllers/index.js';
+
+mongoose.connect(
+    'mongodb+srv://admin:RDxWMFKWxkJ1K8Uh@cluster0.ea1ohvk.mongodb.net/?retryWrites=true&w=majority',
+).then(() => {
+    console.log('Connected to DB');
+}).catch((err) => { 
+    console.log('Failed to connect to DB', err);
+});
 
 const app = express();
 
 const port = 7000;
 
 
-app.listen(port, function () {
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Something bad happened: ', err);
+    }
+
     console.log(`Server listens http://localhost:${port}`);
 });
