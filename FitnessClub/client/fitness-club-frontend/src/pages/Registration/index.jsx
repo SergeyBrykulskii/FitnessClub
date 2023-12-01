d68638e2fb3a3428d7e069879800903f31e4c398
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-12-01 19:07:51 +0300

    Верстка регистрации

--- FitnessClub/client/fitness-club-frontend/src/pages/Registration/index.jsx
+++ FitnessClub/client/fitness-club-frontend/src/pages/Registration/index.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
+import styles from "./Registration.module.scss";
+import { Button } from "../../components";
 export const Registration = () => {
-    return (
-        <div>
-            <h1>Registration</h1>
-        </div>
-    );
+  const isAuth = false;
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+  return (
+    <div>
+      <h1>Registration</h1>
+      <div className={styles.registrationForm}>
+        <form>
+          <div className={styles.TextFild}>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+
+          <div className={styles.TextFild}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+
+          <div className={styles.TextFild}>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+
+          <Button text="Register" type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
