07403d0731514c7be848c30b56889b3a25cf3ec7
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-12-15 23:00:31 +0300

    добавил api

--- FitnessClub/client/fitness-club-frontend/src/pages/Profile/index.jsx
+++ FitnessClub/client/fitness-club-frontend/src/pages/Profile/index.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAuthMe } from "../../redux/slices/auth";
 import styles from "./Profile.module.scss";
+import axios from "axios";
 
 export const Profile = () => {
   const dispatch = useDispatch();
@@ -9,10 +10,35 @@ export const Profile = () => {
     console.log(state);
     return state.auth;
   });
+
   const isLoading = user.userInfoStatus === "loading";
+  const [timezone, setTimezone] = useState("");
+  const [dogImage, setDogImage] = useState("");
+  const [joke, setJoke] = useState("");
 
   useEffect(() => {
     dispatch(fetchAuthMe());
+
+    const currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    setTimezone(currentTimezone);
+
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        setDogImage(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get("https://official-joke-api.appspot.com/random_joke")
+      .then((response) => {
+        setJoke(response.data.setup + " " + response.data.punchline);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   if (isLoading) {
@@ -27,6 +53,9 @@ export const Profile = () => {
           <p>Email: {user.userInfo.email}</p>
           <p>Created At: {user.userInfo.createdAt}</p>
           <p>Updated At: {user.userInfo.updatedAt}</p>
+          <p>Timezone: {timezone}</p>
+          <img src={dogImage} alt="Dog" />
+          <p>{joke}</p>
         </div>
       )}
     </div>
